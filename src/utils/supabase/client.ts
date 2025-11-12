cd4a298a06f30eb25da1f68befd47eba10e7b1b2
이창환 <changhwanlee94@gmail.com>
2025-11-12 15:55:47 +0900

    Feat: Supabase 클라이언트 생성 함수 파일 수정

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,6 +1,16 @@
+/**
+ *  브라우저 환경에서 Supabase 클라이언트를 생성하기 위한 함수
+ *  createBrowserSupabaseClient(): 브라우저에서 사용할 Supabase 클라이언트를 생성하는 함수
+ */
+
+'use client';
+
 import { createBrowserClient } from '@supabase/ssr';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+/**
+ * process.env.NEXT_PUBLIC_SUPABASE_URL! -> 이 url이 null이 아님을 보장
+ * process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! -> 이 anon key가 null이 아님을 보장
+ */
 
-export const createClient = () => createBrowserClient(supabaseUrl!, supabaseKey!);
+// 내 supabase 프로젝트랑 연관된 이 api를 통해서 anon key를 통해서 브라우저 클라이언트가 하나 자동으로 생성이 된다.
+export const createBrowserSupabaseClient = () => createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
