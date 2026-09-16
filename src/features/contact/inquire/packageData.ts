d79021050c8c8d876f7746bab7a1bc24aebf2bdc
LeeChangHwan <chchlee526@gmail.com>
2026-09-17 08:29:19 +0900

    Add : 스포티파이 포트폴리오 추가

--- src/features/contact/inquire/packageData.ts
+++ src/features/contact/inquire/packageData.ts
@@ -1,32 +1,32 @@
 export const packageList = [
-    {
-        package: '싸바리',
-        example: [
-            { name: '2단형', url: '/assets/image/inquire-package/savary1.png' },
-            { name: '3단형', url: '/assets/image/inquire-package/savary2.png' },
-            { name: '서랍형', url: '/assets/image/inquire-package/savary3.png' },
-            { name: '표지+지함형', url: '/assets/image/inquire-package/savary4.png' },
-        ],
-    },
-    {
-        package: '단상자',
-        example: [
-            { name: 'A형', url: '/assets/image/inquire-package/dan1.png' },
-            { name: 'B형', url: '/assets/image/inquire-package/dan2.png' },
-            { name: 'Y형 상하', url: '/assets/image/inquire-package/dan3.png' },
-            { name: '슬리브형', url: '/assets/image/inquire-package/dan4.png' },
-            { name: 'G형', url: '/assets/image/inquire-package/dan5.png' },
-            { name: 'R형', url: '/assets/image/inquire-package/dan6.png' },
-        ],
-    },
-    {
-        package: '쇼핑백',
-        example: [
-            { name: '일반형 쇼핑백', url: '/assets/image/inquire-package/shop1.png' },
-            { name: '매립형 쇼핑백', url: '/assets/image/inquire-package/shop2.png' },
-            { name: '타공형 쇼핑백', url: '/assets/image/inquire-package/shop3.png' },
-        ],
-    },
-    { package: '커스텀박스', example: [] },
-    { package: '기타', example: [] },
+  {
+    package: '싸바리',
+    example: [
+      { name: '2단형', url: '/assets/image/inquire-package/savary1.png' },
+      { name: '3단형', url: '/assets/image/inquire-package/savary2.png' },
+      { name: '서랍형', url: '/assets/image/inquire-package/savary3.png' },
+      { name: '표지+지함형', url: '/assets/image/inquire-package/savary4.png' },
+    ],
+  },
+  {
+    package: '단상자',
+    example: [
+      { name: 'A형', url: '/assets/image/inquire-package/dan1.png' },
+      { name: 'B형', url: '/assets/image/inquire-package/dan2.png' },
+      { name: 'Y형 상하', url: '/assets/image/inquire-package/dan3.png' },
+      { name: '슬리브형', url: '/assets/image/inquire-package/dan4.png' },
+      { name: 'G형', url: '/assets/image/inquire-package/dan5.png' },
+      { name: 'R형', url: '/assets/image/inquire-package/dan6.png' },
+    ],
+  },
+  {
+    package: '쇼핑백',
+    example: [
+      { name: '일반형 쇼핑백', url: '/assets/image/inquire-package/shop1.png' },
+      { name: '매립형 쇼핑백', url: '/assets/image/inquire-package/shop2.png' },
+      { name: '타공형 쇼핑백', url: '/assets/image/inquire-package/shop3.png' },
+    ],
+  },
+  { package: '커스텀박스', example: [] },
+  { package: '기타', example: [] },
 ];
